7738d33eba242d512b2ca143d8cab05ecf4953c1
Babur Usenakunov <bob.usenakunov@gmail.com>
2013-07-05 00:40:18 +0600

    Basic flow customization

--- app/controllers/spree/omniauth_callbacks_controller.rb
+++ app/controllers/spree/omniauth_callbacks_controller.rb
@@ -3,36 +3,40 @@ class Spree::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   include Spree::Core::ControllerHelpers::Order
   include Spree::Core::ControllerHelpers::Auth
 
+  layout false
+
   def self.provides_callback_for(*providers)
     providers.each do |provider|
       class_eval %Q{
         def #{provider}
           if request.env["omniauth.error"].present?
-            flash[:error] = t("devise.omniauth_callbacks.failure", :kind => auth_hash['provider'], :reason => t(:user_was_not_valid))
-            redirect_back_or_default(root_url)
+            render 'failure'
             return
           end
 
           authentication = Spree::UserAuthentication.find_by_provider_and_uid(auth_hash['provider'], auth_hash['uid'])
 
           if authentication.present?
-            flash[:notice] = "Signed in successfully"
-            sign_in_and_redirect :spree_user, authentication.user
+            sign_in :spree_user, authentication.user
+            @url = after_sign_in_url
+            render 'success'
           elsif spree_current_user
             spree_current_user.apply_omniauth(auth_hash)
-            spree_current_user.save!
-            flash[:notice] = "Authentication successful."
-            redirect_back_or_default(account_url)
+            if spree_current_user.save
+              render 'add_success'
+            else
+              render 'failure'
+            end
           else
             user = Spree::User.find_by_email(auth_hash['info']['email']) || Spree::User.new
             user.apply_omniauth(auth_hash)
             if user.save
-              flash[:notice] = "Signed in successfully."
-              sign_in_and_redirect :spree_user, user
+              sign_in :spree_user, user
+              @url = after_sign_in_url
+              render 'success'
             else
               session[:omniauth] = auth_hash.except('extra')
-              flash[:notice] = Spree.t(:one_more_step, :kind => auth_hash['provider'].capitalize)
-              redirect_to new_spree_user_registration_url
+              render 'failure'
             end
           end
 
@@ -51,8 +55,7 @@ class Spree::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end
 
   def failure
-    set_flash_message :alert, :failure, :kind => failed_strategy.name.to_s.humanize, :reason => failure_message
-    redirect_to spree.login_path
+    render
   end
 
   def passthru
@@ -62,4 +65,10 @@ class Spree::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def auth_hash
     request.env["omniauth.auth"]
   end
-end
+
+  private
+
+    def after_sign_in_url
+      session.delete('spree_user_return_to') || root_url
+    end
+end 
