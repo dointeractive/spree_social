29ee98cfe19015943fc1e3c3f0631e089f639896
Babur Usenakunov <bob.usenakunov@gmail.com>
2013-07-05 01:42:41 +0600

    Retrieving email, first name ans last name

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -4,9 +4,12 @@ Spree.user_class.class_eval do
   devise :omniauthable
 
   def apply_omniauth(omniauth)
-    if omniauth['provider'] == "facebook"
-      self.email = omniauth['info']['email'] if email.blank?
+    if email.blank?
+      self.email = omniauth['info']['email'].presence || "#{self.class.generate_token(:persistence_token)}@temp.temp"
     end
+    self.first_name = omniauth['info']['first_name'] if first_name.blank? && omniauth['info']['first_name'].present?
+    self.last_name = omniauth['info']['last_name'] if last_name.blank? && omniauth['info']['last_name'].present?
+    
     user_authentications.build(:provider => omniauth['provider'], :uid => omniauth['uid'])
   end
 
